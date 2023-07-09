92f3a33bb7db93619f7f2177aa41314987e49c49
Waqar Kabir <waqarkabi@hotmail.com>
2023-07-09 06:35:10 +0500

    Status Result implementation

--- WebAPI/Controllers/RecipesController.cs
+++ WebAPI/Controllers/RecipesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -11,9 +12,37 @@ namespace WebAPI.Controllers
         public IActionResult GetRecipes()
         {
             string[] recipes = { "Pizza", "Curry", "Oxtail"};
-            if (recipes.Any())
+            if (!recipes.Any())
                 return NotFound();
             return Ok(recipes);
+        }
+
+        [HttpPost]
+        public IActionResult CreateRecipe()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Created("", new {});
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteRecipes(int id) 
+        {
+            bool badThingsHappened = false;
+            if (badThingsHappened)
+                return BadRequest();
+
+            return NoContent();
+        }
+
+        [HttpPut]
+        public IActionResult EditRecipe()
+        {
+            if (!ModelState.IsValid)
+                return NotFound();
+
+            return NoContent();
         }
     }
 }
