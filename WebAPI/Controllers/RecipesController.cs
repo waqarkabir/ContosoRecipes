c230e1a781d23dd9822e2c63e677c54df0be58c3
Waqar Kabir <waqarkabi@hotmail.com>
2023-07-09 05:41:27 +0500

    First API Endpoint

---
+++ WebAPI/Controllers/RecipesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetRecipes()
+        {
+            string[] recipes = { "Pizza", "Curry", "Oxtail"};
+            return recipes;
+        }
+    }
+}
